861d47930acd79f89c8e2f2bf072b282475c7af9
coroutine <89150791+Paupxx@users.noreply.github.com>
2024-04-13 20:21:22 -0700

    Update autokill.lua

--- autokill.lua
+++ autokill.lua
@@ -1,4 +1,4 @@
-task.wait()
+--task.wait()
 --if not game:IsLoaded() then
 --	game.Loaded:Wait()
 --end
@@ -19,6 +19,7 @@ local function tp()
 			end
 		end
 	end
+
 	if #servers > 0 then
 		if not istping then
 			istping = true
@@ -30,56 +31,51 @@ local function tp()
 	end
 end
 
-table.insert(cons, game.Players.PlayerRemoving:Connect(function(p)
-	if #game.Players:GetPlayers() <= 42352 then 
-		tp()
-	end
-end))
-
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
 
 table.insert(cons, game:GetService("RunService").RenderStepped:Connect(function()
-    if Player and Player.Character then 
+    local Tool = Player.Character:FindFirstChildOfClass("Tool")
+	if Tool ~= nil then 
+		Tool:Activate()
+		local Handle = Tool.Handle
+		for i,Targ in pairs(Players:GetPlayers()) do 
+			if Targ and Targ ~= Player and Targ.Character and Targ.Character:FindFirstChildOfClass("Humanoid") and Targ.Character:FindFirstChildOfClass("Humanoid"):GetState() ~= Enum.HumanoidStateType.Dead then 
+				for i,v in pairs(Targ.Character:GetChildren()) do 
+					if v:IsA("BasePart") then 
+						firetouchinterest(Handle, v, 0)
+						firetouchinterest(Handle, v, 1)
+					end
+				end
+			end
+		end
+	else
+		if Player.Backpack:FindFirstChildOfClass("Tool") then 
+			Player.Backpack:FindFirstChildOfClass("Tool").Parent = Player.Character
+		end
+	end
+	Player.Character.HumanoidRootPart.CFrame = CFrame.new(31, 5000, 212)
+	task.spawn(function()
 		Player.Character.HumanoidRootPart.CFrame = CFrame.new(31, 5000, 212)
-        for i,v in pairs(Player.Character:GetDescendants()) do 
+		for i,v in pairs(Player.Character:GetDescendants()) do 
 			if v:IsA("BasePart") then 
 				v.Velocity = Vector3.new(0,0,0)
 				v.RotVelocity = Vector3.new(0,0,0)
 			end
 		end
-		local Handle = Player.Character:FindFirstChildOfClass("Tool") and Player.Character:FindFirstChildOfClass("Tool"):FindFirstChild("Handle")
-        if Handle then 
-			Handle.Parent:Activate()
-			for i,Targ in pairs(game.Players:GetPlayers()) do 
-				if Targ and Targ ~= Player and Targ.Character and Targ.Character:FindFirstChildOfClass("Humanoid") and Targ.Character:FindFirstChildOfClass("Humanoid"):GetState() ~= Enum.HumanoidStateType.Dead then 
-					if not Targ.Character:FindFirstChildOfClass("ForceField") then 
-						for i,v in pairs(Targ.Character:GetChildren()) do 
-							if v:IsA("BasePart") then 
-								firetouchinterest(Handle, v, 0)
-								firetouchinterest(Handle, v, 1)
-							end
-						end
-					end
-				end
-			end
-		else
-			if Player.Backpack:FindFirstChildOfClass("Tool") then 
-				Player.Backpack:FindFirstChildOfClass("Tool").Parent = Player.Character
-			end
-        end
-    end
+	end)
 	if #game.Players:GetPlayers() <= 23422 then 
 		tp()
 	end
 end))
 
-local queue = false
-
-table.insert(cons, game.Players.LocalPlayer.OnTeleport:Connect(function()
+table.insert(cons, Player.OnTeleport:Connect(function()
 	if not queue then
 		queue = true
-		for i,v in pairs(cons) do v:Disconnect() v = nil end
+		for i,v in pairs(cons) do 
+			v:Disconnect() 
+			v = nil 
+		end
 		queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/Paupxx/Scripts2/main/autokill.lua"))()')
 	end
 end))
